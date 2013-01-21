01f82fef6d138ea5e708ba31e762505440c7f17a
Christian Ledermann <christian.ledermann@gmail.com>
2013-01-21 19:35:15 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,24 @@ version = '0.2'
 setup(name='collective.geo.flexitopic',
       version=version,
       description="Flexitopic for collective.geo",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Topic :: Scientific/Engineering :: GIS",
         ],
       keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      author='Christian Ledermann',
+      author_email='christian.ledermann@gmail.com',
+      url='http://plone.org/products/collective.geo.flexitopic',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.geo'],
